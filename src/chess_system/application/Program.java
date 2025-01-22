89c20403e934e3e72d0861ca42ff3b982e084c8e
KaikSAlves <souzaalveskaik@gmail.com>
2025-01-22 07:12:35 -0300

    Starting to implement Board and Piece

--- src/chess_system/application/Program.java
+++ src/chess_system/application/Program.java
@@ -1,10 +1,11 @@
 package chess_system.application;
 
+import chess_system.application.boardgame.Board;
 import chess_system.application.boardgame.Position;
 
 public class Program {
 	public static void main(String[] args) {
-		Position p = new Position(3,5);
-		System.out.println(p);
+		
+		Board board = new Board(8, 8);
 	}
 }
